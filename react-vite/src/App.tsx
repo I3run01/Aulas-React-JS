f1c19256b75685a78424113030cfef15385e3613
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-05-04 15:52:29 -0300

    commit

--- react-vite/src/App.tsx
+++ react-vite/src/App.tsx
@@ -1,43 +1,18 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App =() => {
+  let n1: number = 10
+  let n2: number = 3
+  let link = 'https://alunos.b7web.com.br/curso/reactjs/expressoes-no-jsxts'
+
+  function somar(n1: number, n2: number): number {
+    return n1 + n2
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+      <p>Somar: {somar(n1, n2)}!</p>
+      <a href={link}>clique aqui</a>
     </div>
   )
 }
