f497b81db651e42d6ac7fe5d7955b737fa7a77fd
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-07 20:36:42 -0300

    c

--- Modulo 3 - Listas e Estilização/src/App.tsx
+++ Modulo 3 - Listas e Estilização/src/App.tsx
@@ -1,8 +1,49 @@
+import { Header } from "./components/Header"
+import { useState } from "react"
+
 const App =() => {
+
+  //Estelizando inline dinamicamente
+  const [clicked, setClicked] = useState(false)
+  const [padding, setPadding] = useState(0)
+
+  const handleClick = () => {
+    setClicked(true)
+    setPadding(20)
+  }
+
+  
+  
   
   return (
     <div>
-   
+
+      <Header 
+        title='Estilizando inline' ></Header>
+      <button style={{ 
+        backgroundColor: 'red',
+        color: '#fff',
+        border: 0
+      }}>Clique aqui</button>
+
+      
+      <Header 
+        title="Estilizando inline dinamicamente"></Header>
+      <button
+        onClick={handleClick}
+        style={{ 
+          backgroundColor: clicked ? 'blue' : 'red',
+          color: '#fff',
+          border: 0,
+          padding: padding, //Por ser o mesmo nome, não precisa colocar padding duas vezes
+        }}>Clique aqui</button>
+
+        <Header 
+          title="Estilizando com StyleShhet"></Header>
+          
+      
+
+
     </div>
     
   )
