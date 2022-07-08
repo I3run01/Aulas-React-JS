6b8d81925a725a4a8dc5c67c7aee7c0f96dea8dd
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-08 15:50:39 -0300

    c

--- Modulo 3 - Listas e Estilização/Styled Components/src/App.tsx
+++ Modulo 3 - Listas e Estilização/Styled Components/src/App.tsx
@@ -1,49 +1,11 @@
-import { useState } from "react"
-import { Header } from "./components/Header"
-import { Botao } from "./components/Botao"
+import * as C from './AppAtyles'
 
 const App =() => {
-
-  //Estelizando inline dinamicamente
-  const [clicked, setClicked] = useState(false)
-  const [padding, setPadding] = useState(0)
-
-  const handleClick = () => {
-    setClicked(true)
-    setPadding(20)
-  }
-
-  
-  
-  
   return (
-    <div>
-
-      <Header 
-        title='Estilizando inline' ></Header>
-      <button style={{ 
-        backgroundColor: 'red',
-        color: '#fff',
-        border: 0
-      }}>Clique aqui</button>
-
-      
-      <Header 
-        title="Estilizando inline dinamicamente"></Header>
-      <button
-        onClick={handleClick}
-        style={{ 
-          backgroundColor: clicked ? 'blue' : 'red',
-          color: '#fff',
-          border: 0,
-          padding: padding, //Por ser o mesmo nome, não precisa colocar padding duas vezes
-        }}>Clique aqui</button>
-
-        <Header 
-          title="Estilizando com StyleShhet"></Header>
-          <Botao />
-
-    </div>
+    <C.Container>
+      Texto do componente
+      <C.Botao>Clique aqui</C.Botao>
+    </C.Container>
     
   )
 }
