92dc3282fcf9457ef8a7bc280d423cfb68aef2ed
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-08 22:29:35 -0300

    c

--- Modulo 3 - Listas e Estilização/Styled Components/src/App.tsx
+++ Modulo 3 - Listas e Estilização/Styled Components/src/App.tsx
@@ -1,10 +1,18 @@
 import * as C from './AppStyles'
+import { Header } from './components/Header'
 
 const App =() => {
   return (
-    <C.Container>
-      Texto do componente
-      <C.Botao>Clique aqui</C.Botao>
+    
+    //Props dinâmicas
+    <C.Container bgColor='purple'>
+      <span> Texto do componente</span>
+
+      <a href="" className='link'>Link qualquer</a>
+
+
+      <C.Botao bg='red'>Clique aqui</C.Botao>
+      <C.Botao bg='green' small>Botão pequeno</C.Botao>
     </C.Container>
     
   )
--- Modulo 3 - Listas e Estilização/Styled Components/src/AppStyles.ts
+++ Modulo 3 - Listas e Estilização/Styled Components/src/AppStyles.ts
@@ -1,11 +1,45 @@
 import styled from "styled-components";
 
-export const Botao = styled.button`
-  font-size: 40px;
-`
+type ContainerProps = {
+  bgColor: string
+}
 
-export const Container = styled.div`
-  background-color: blue;
+export const Container = styled.div<ContainerProps>`
+  max-width: 600px;
+  margin: auto;
+  background-color: ${props => props.bgColor};
   color: white;
-`
+  padding: 20px;
 
+  span {
+    font-weight: bold;
+    color: #000;
+  }
+
+  .link {
+    color: white;
+
+    &:hover {
+      color: red;
+    }
+  }
+
+  @media (max-width: 500px) {
+    background-color: gray;
+    flex-direction: column;
+
+    span {
+      color: blue;
+    }
+  }
+
+`
+type BotaoProps = {
+  bg: string
+  small?: boolean
+}
+export const Botao = styled.button<BotaoProps>`
+  font-size: ${props => props.small ? '15px' : '30px'};
+  background-color: ${props => props.bg};
+  
+`
