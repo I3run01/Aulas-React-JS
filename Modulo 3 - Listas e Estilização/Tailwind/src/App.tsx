90b7bb4205c42971cd55caf6be97288396c792dd
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-11 08:15:30 -0300

    C

--- Modulo 3 - Listas e Estilização/Tailwind/src/App.tsx
+++ Modulo 3 - Listas e Estilização/Tailwind/src/App.tsx
@@ -3,18 +3,7 @@ import { Header } from './components/Header'
 
 const App =() => {
   return (
-    
-    //Props dinâmicas
-    <C.Container bgColor='purple'>
-      <span> Texto do componente</span>
-
-      <a href="" className='link'>Link qualquer</a>
-
-
-      <C.Botao bg='red'>Clique aqui</C.Botao>
-      <C.Botao bg='green' small>Botão pequeno</C.Botao>
-    </C.Container>
-    
+    <div>...</div>
   )
 }
 
