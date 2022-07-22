101c4865e25ad4b619f76a0493a00e138a0f28fc
I3run01 <brunnooa.v@gmail.com>
2022-07-22 12:03:38 -0300

    c

--- Modulo 5 - Ciclo de vida, requisição e hooks/UseEffect/src/App.tsx
+++ Modulo 5 - Ciclo de vida, requisição e hooks/UseEffect/src/App.tsx
@@ -7,8 +7,7 @@ const App =() => {
 
   useEffect(() => {
     setFullName(`${name} ${lastName}`)
-  }), [name, lastName]
-
+  }, [name, lastName])
   const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
     setName( e.target.value )
   }
