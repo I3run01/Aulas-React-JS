1cfd039571940b49cdf18029057e5aaaa5b42409
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-25 20:57:10 -0300

    c

--- Modulo 7 - React Router (v6)/src/App.tsx
+++ Modulo 7 - React Router (v6)/src/App.tsx
@@ -1,44 +1,20 @@
-import { Routes, Route } from "react-router-dom"
-import { Home } from "./pages/Home"
-import { About } from "./pages/About"
-import { AboutBonieky } from "./pages/AboutBonieky" 
-import { AboutPedro } from "./pages/AboutPedro" 
-import { AboutItem } from "./pages/AboutItem"
-import { NotFound } from "./pages/Notfound"
-import { RequireAuth } from "./RequireAuth"
+import { MainRoutes } from "./routes/MainRoutes"
 
 const App =() => {
   return (
     <div>
+      
       <header>
         <h1>Titulo do site</h1>
       </header>
       
       <br />
       <div>
-
-        <Routes>
-          <Route path="/" element={<Home />}></Route>
-
-          /* O RequireAuth vai validar se o meu usuário poderá acessar o About */
-          <Route path="/sobre" element={
-            <RequireAuth>
-              <About/>
-            </RequireAuth>
-          }/>
-
-          <Route path="/sobre/bonieky" element={<AboutBonieky />}></Route>
-          <Route path="/sobre/pedro" element={<AboutPedro />}></Route>
-          
-          /* Rotas dinamicas -- O parametro vem depois dos ":", neste cado o meu parametro é o slug */
-          <Route path="/sobre/:slug" element={<AboutItem/>}></Route>
-
-          <Route path="*" element={<NotFound />}></Route>
-        </Routes>
-
-
+        <MainRoutes/>
       </div>
+
       <br />
+
       <footer>
         Todos os direitos reservados
       </footer>
