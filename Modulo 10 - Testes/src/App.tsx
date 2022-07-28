5dc31f531f3ce6ac162d4e42569c37b82daa68f9
Bruno Alves Vieira <94388068+I3run01@users.noreply.github.com>
2022-07-28 20:15:39 -0300

    c

--- Modulo 10 - Testes/src/App.tsx
+++ Modulo 10 - Testes/src/App.tsx
@@ -1,11 +1,25 @@
-const App =() => {
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import { Header } from './components/Header';
+import { CarList } from './pages/CarList';
+import { NotFound } from './pages/NotFound';
+import { EditCar } from './pages/EditCar';
 
+function App() {
   return (
-      <div>
+    <div className="app">
+      <Header />
 
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<CarList />} />
+          <Route path="/car" element={<EditCar />} />
+          <Route path="/car/:id" element={<EditCar />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
